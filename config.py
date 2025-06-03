3c5961290776951100788ec6bf6e78121fb2287b
Gagan Chauhan <81284966+GAGGZ1@users.noreply.github.com>
2025-06-03 16:41:35 +0530

    Fixed the code

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
-MYSQL_HOST = 'localhost'
-MYSQL_USER = 'root'  # default MySQL username
-MYSQL_PASSWORD = 'xxxxx'  # set your MySQL password here
-MYSQL_DB = 'doctor_appointment'
+# MYSQL_HOST = 'localhost'
+# MYSQL_USER = 'root'  # default MySQL username
+# MYSQL_PASSWORD = 'xxxxx'  # set your MySQL password here
+# MYSQL_DB = 'doctor_appointment'
 
 
 # Stripe keys
 
-STRIPE_PUBLIC_KEY = 'xxxx'
-STRIPE_SECRET_KEY = 'xxxx'
+# STRIPE_PUBLIC_KEY = 'xxxx'
+# STRIPE_SECRET_KEY = 'xxxx'
